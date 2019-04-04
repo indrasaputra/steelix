18947fadd2927b005c8ce80972f2c106d62bc9c5
Indra Saputra <flame.fire99@gmail.com>
2019-04-04 17:50:19 +0700

    create empty http client

--- http_client.go
+++ http_client.go
@@ -1,2 +1,7 @@
 // Package steelix wraps http client and makes it more resilient.
 package steelix
+
+// HTTPClient wraps native golang http client.
+// In addition, it provides retry strategy.
+type HTTPClient struct {
+}
