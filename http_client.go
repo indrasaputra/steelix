2dfd73fe17f3c6b14ef0759243fafb03b09dbc28
Indra Saputra <flame.fire99@gmail.com>
2019-04-04 18:01:59 +0700

    create backoff interface

--- http_client.go
+++ http_client.go
@@ -1,6 +1,18 @@
 // Package steelix wraps http client and makes it more resilient.
 package steelix
 
+import "time"
+
+// Backoff is a contract for implementing backoff strategy.
+type Backoff interface {
+	// NextInterval returns the interval for the subsequent requests.
+	NextInterval() time.Duration
+}
+
+// ClientConfig holds any configuration needed by HTTPClient.
+type ClientConfig struct {
+}
+
 // HTTPClient wraps native golang http client.
 // In addition, it provides retry strategy.
 type HTTPClient struct {
