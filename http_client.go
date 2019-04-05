730c2cbb5287fcf806638afa3c71f4e34c1b5f27
Indra Saputra <flame.fire99@gmail.com>
2019-04-05 10:01:17 +0700

    Skeleton (#1)
    * create http client
    
    * create empty http client
    
    * create backoff interface
    
    * include backoff and max retry as config
    
    * add default max retry cons

---
+++ http_client.go
@@ -0,0 +1,32 @@
+// Package steelix wraps http client and makes it more resilient.
+package steelix
+
+import "time"
+
+const (
+	// DefaultMaxRetry is the default value for max retry.
+	DefaultMaxRetry = 0
+)
+
+// Backoff is a contract for implementing backoff strategy.
+type Backoff interface {
+	// NextInterval returns the interval for the subsequent requests.
+	NextInterval() time.Duration
+}
+
+// ClientConfig holds any configuration needed by HTTPClient.
+type ClientConfig struct {
+	// Backoff is backoff strategy.
+	Backoff Backoff
+	// MaxRetry sets how many times a request should be tried if error happens.
+	// To make it clear, this is how the request life cycle.
+	// Say we set MaxRetry to 1.
+	// First, a request will be launched. If an error occurred, then the request will be tried once.
+	// In other words, by setup MaxRetry to 1, at most there will be two trials.
+	MaxRetry int
+}
+
+// HTTPClient wraps native golang http client.
+// In addition, it provides retry strategy.
+type HTTPClient struct {
+}
