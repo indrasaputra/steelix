f4d56870e8f5ec46af17e6444cfe77083125b6ce
Indra Saputra <flame.fire99@gmail.com>
2019-04-05 11:29:02 +0700

    assert constructor not returning nil

--- http_client_test.go
+++ http_client_test.go
@@ -1,11 +1,26 @@
 package steelix_test
 
 import (
+	"net/http"
+	"testing"
 	"time"
+
+	"github.com/indrasaputra/steelix"
+	"github.com/stretchr/testify/assert"
 )
 
 type mockBackoff struct{}
 
 func (m mockBackoff) NextInterval() time.Duration {
 	return 0
+}
+
+func TestNewHTTPClient(*testing.T) {
+	config := &steelix.ClientConfig{
+		Backoff:  mockBackoff{},
+		MaxRetry: 0,
+	}
+	client := steelix.NewHTTPClient(http.DefaultClient, config)
+
+	assert.NotNil(client)
 }
