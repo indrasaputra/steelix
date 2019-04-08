af043eca43cf7b7b5597f52403e210b648e257b2
Indra Saputra <flame.fire99@gmail.com>
2019-04-08 14:41:15 +0700

    implement Do method for http breaker client

--- circuit_breaker_client.go
+++ circuit_breaker_client.go
@@ -1,7 +1,11 @@
 package steelix
 
 import (
+	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/sony/gobreaker"
 )
@@ -68,7 +72,32 @@ func NewHTTPBreakerClient(client *HTTPClient, config *BreakerConfig) *HTTPBreake
 // When the ClientConfig is set, it also apply all resiliency strategies
 // configured there, such as retry strategy.
 func (h *HTTPBreakerClient) Do(req *http.Request) (*http.Response, error) {
+	var resp *http.Response
+	var err error
 
+	for i := uint32(0); i < h.client.config.MaxRetry; i++ {
+		if resp != nil {
+			io.Copy(ioutil.Discard, resp.Body)
+			resp.Body.Close()
+		}
+
+		req.Header.Set("X-Steelix-Retry", fmt.Sprintf("%d", i))
+		tmp, err := h.breaker.Execute(func() (interface{}, error) {
+			return h.client.client.Do(req)
+		})
+		if err != nil {
+			time.Sleep(h.client.config.Backoff.NextInterval())
+			continue
+		}
+		resp := tmp.(*http.Response)
+		if resp.StatusCode >= 500 {
+			time.Sleep(h.client.config.Backoff.NextInterval())
+			continue
+		}
+		break
+	}
+
+	return resp, err
 }
 
 func createBreakerSettings(config *BreakerConfig) gobreaker.Settings {
