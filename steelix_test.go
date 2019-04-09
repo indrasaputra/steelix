d8233a035d2e70d94ade7a6206b5a53b890aeb28
Indra Saputra <flame.fire99@gmail.com>
2019-04-09 14:37:09 +0700

    test client with retry but no breaker

--- steelix_test.go
+++ steelix_test.go
@@ -1,10 +1,12 @@
 package steelix_test
 
 import (
+	"net/http"
 	"testing"
 	"time"
 
 	"github.com/indrasaputra/steelix"
+	"github.com/stretchr/testify/assert"
 )
 
 type mockBackoff struct{}
@@ -12,7 +14,10 @@ type mockBackoff struct{}
 func (mockBackoff) NextInterval() time.Duration { return 0 }
 
 func TestNewClient(t *testing.T) {
-
+	// --- with retry, without breaker ---
+	rc := createRetryConfig(5)
+	client := steelix.NewClient(http.DefaultClient, rc, nil)
+	assert.NotNil(t, client)
 }
 
 func createRetryConfig(n uint32) *steelix.RetryConfig {
