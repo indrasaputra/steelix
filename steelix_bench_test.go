8efa8668f7203c0259a19b267292af2293a51589
Indra Saputra <flame.fire99@gmail.com>
2019-04-09 18:31:50 +0700

    Benchmark (#10)
    * benchmark steelix client
    
    * use doer interface for benchmark client
    
    * benchmark steelix client with breaker
    
    * benchmark http client

---
+++ steelix_bench_test.go
@@ -0,0 +1,45 @@
+package steelix_test
+
+import (
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/indrasaputra/steelix"
+)
+
+type doer interface {
+	Do(*http.Request) (*http.Response, error)
+}
+
+func BenchmarkHTTPClient(b *testing.B) {
+	benchmarkClient(b, http.DefaultClient)
+}
+
+func BenchmarkSteelixClient_Do_WithRetry(b *testing.B) {
+	rc := createRetryConfig(5)
+	client := steelix.NewClient(http.DefaultClient, rc, nil)
+	benchmarkClient(b, client)
+}
+
+func BenchmarkSteelixClient_Do_WithBreaker(b *testing.B) {
+	client := steelix.NewClient(http.DefaultClient, nil, createConsecutiveBreakerConfig())
+	benchmarkClient(b, client)
+}
+
+func benchmarkClient(b *testing.B, client doer) {
+	server := httptest.NewServer(http.HandlerFunc(createOkHandler()))
+	defer server.Close()
+
+	req, _ := http.NewRequest(http.MethodGet, server.URL, nil)
+
+	for i := 0; i < b.N; i++ {
+		resp, _ := client.Do(req)
+		if resp != nil {
+			io.Copy(ioutil.Discard, resp.Body)
+			resp.Body.Close()
+		}
+	}
+}
